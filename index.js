fe202e5544f91de54961ce78aa43bb9ffe9b1e51
Terminator1601 <rishisharma4201@gmail.com>
2024-03-30 11:22:49 +0530

    add axios

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5000;
+const axios =require('axios')
 
 // Middleware
 // app.use(cors());
